823c862217168e6a2ef69bbd2425fc15568c7f02
Rakaris <dainis@a2d.lv>
2014-04-08 20:56:07 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -37,4 +37,4 @@ for entry in dom.cssselect('.mep_details'):
     'name': entry.cssselect('.mep_name')[0].text_content(),
     }
     print post
-scraperwiki.sqlite.save(unique_keys=['3I2TVQmEpXaT2vC89Ucx'], data={"3I2TVQmEpXaT2vC89Ucx": "Rakaris", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['EirodeputātuSaraksts'], data={"EirodeputātuSaraksts": "Rakaris", "PatsSevSaimnieks": "Entuziasts"})
